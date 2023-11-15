fed09471fee64a1b646dc2d69f7b9b0a75fd327b
mia <yanjiaxin96@gmail.com>
2023-11-15 11:53:36 -0800

    modified addInitialData func to solve duplication

--- firebase/firestore.js
+++ firebase/firestore.js
@@ -9,13 +9,30 @@ import {
   doc,
   updateDoc,
   Timestamp,
+  query, 
+  where, 
+  getDocs
 } from "@firebase/firestore";
 import { db } from "./firebaseSetup";
 
 
+
 export async function addInitialDataToFirestore(data) {
   try {
-    await addDoc(collection(db, "traillist"), data);
+    const { trailTitle } = data;
+
+    // Check if a document with the given trailTitle already exists
+    const trailsRef = collection(db, 'traillist');
+    const q = query(trailsRef, where('trailTitle', '==', trailTitle));
+    const querySnapshot = await getDocs(q);
+
+    if (querySnapshot.empty) {
+      // No existing document found, add the new one
+      await addDoc(trailsRef, data);
+      console.log(`Document with trailTitle ${trailTitle} added to Firestore.`);
+    } else {
+      console.log(`Document with trailTitle ${trailTitle} already exists in Firestore. Skipped.`);
+    }
   } catch (err) {
     logError(err);
   }
