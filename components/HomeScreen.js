5d6b7fc940f777dc53a6e36cbd4666c69f935d59
mia <yanjiaxin96@gmail.com>
2023-11-14 21:07:14 -0800

    home screen and detail screen demo

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,19 +1,113 @@
 import React from 'react'
-import { StyleSheet, View, Text, Button } from 'react-native'
+import { Pressable, FlatList, Image, ImageBackground, StyleSheet, View, Text, Button } from 'react-native'
+import { backGroundImage } from '../styles';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Icon from 'react-native-vector-icons/FontAwesome';
+import TopTrailsItem from './TopTrailsItem';
 
 const Stack = createNativeStackNavigator();
-const HomeScreen = ({ navigation }) => {
-  const detailsHandler = () => {
-		navigation.navigate('Details');
+const HomeScreen = ({ navigation, route }) => {
+  const list = [
+    {name: 'Grouse Grind', rate: 4.5, imageUri: "https://insidevancouver.ca/wp-content/uploads/2013/08/Grousegrind2.jpeg"},
+    {name: "St Mark's Summit", rate: 4.0, imageUri: 'https://www.insidevancouver.ca/wp-content/uploads/2021/08/vlad-d-ytAt9TT-X38-unsplash-664x434.jpg'},
+    {name: "Sea To Summit", rate: 4.0, imageUri: 'https://grindaholic.ca/wp-content/uploads/2020/06/Sea-to-summit-trail.jpg'},
+    {name: "Quarry Rock", rate: 4.5, imageUri: 'https://images.dailyhive.com/20170825082201/north-vancouver-quarry-rock-hiking-deep-cove.jpg'},
+    {name: "Tunnel Bluffs", rate: 4.7, imageUri: 'https://cdn-assets.alltrails.com/uploads/photo/image/62578510/b7c02601ffc90c343f6449758f7676f8.jpg'}
+  ]
+
+  const detailsHandler = (pressedItem) => {
+		navigation.navigate('Details', {pressedItem});
 	}
 
+  const renderItem = ({ item }) => (
+    <TopTrailsItem item={item} itemPressHandle={detailsHandler} />
+  );
+
+  const tailorPressedHandle = () => {
+    console.log("tailor pressed")
+  }
+
   return (
-    <View>
-      <Text>Home</Text>
-      <Button title="Details" onPress={detailsHandler}/>
-    </View>
+    <ImageBackground
+			source={require('../assets/background_image.jpeg')}
+			style={ backGroundImage }
+		>
+			<View style={styles.container}>
+				<Image 
+          source={require('../assets/trail_logo.png')} 
+          style={styles.image}
+        />
+        <Text style={styles.listTitle}>Top 5 Trails</Text>
+        <View style={styles.listContainer}>
+          <FlatList
+            data={list}
+            horizontal={true}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.name}
+            showsHorizontalScrollIndicator={false} 
+          />
+        </View>
+        <Pressable style={styles.tailorContainer} onPress={tailorPressedHandle}>
+          <Image
+            source={require('../assets/tailor_image.jpeg')}
+            style={styles.tailorImage}
+          />
+          <View style={styles.tailorTextContainer}>
+            <Text style={styles.tailorText}>Tailor Your Trails!</Text>
+          </View>
+        </Pressable>
+			</View>
+		</ImageBackground>
   )
 }
+const styles = StyleSheet.create({
+	container:{
+		flex: 1,
+		backgroundColor: 'rgba(255,255,255,0.7)', 
+		justifyContent: 'flex-start',
+  	alignItems: 'flex-start',
+    width: '100%',
+	},
+	image: {
+    marginLeft: 120,
+    width: 200,
+    height: 130,
+    resizeMode: 'contain',
+  },
+  listTitle: {
+    fontSize: 20,
+    marginLeft:15,
+    marginBottom: 8,
+    textAlign: 'left'
+  },
+  listContainer:{
+    marginHorizontal: 10,
+    flexDirection: 'row'
+  },
+  tailorContainer:{
+    position: 'relative',
+    width: '92%',
+    marginLeft: 20,
+    marginRight: 20,
+    marginVertical: 20,
+  },
+  tailorImage:{
+    width: '100%',
+    marginTop: 15,
+    height: 240,
+    borderRadius: 15,
+  },
+  tailorTextContainer:{
+    position: 'absolute',
+    bottom: 40, 
+    right: 20,
+  },
+  tailorText:{
+    fontSize: 35,
+    color: 'white',
+    fontWeight: 'bold'
+  }
+
+})
 
 export default HomeScreen
---
+++ components/TopTrailsItem.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import { Image, Text, View, StyleSheet, Pressable } from 'react-native'
+import {themeBackgroundColor, buttonborderRadius, themeTintColor, buttonFontSize, buttonInactiveColor } from '../styles'
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export const TopTrailsItem = (props) => {
+	console.log(props);
+	const rate = props.item.rate;
+	const fullStars = Math.floor(rate);
+  const halfStar = rate - fullStars >= 0.5 ? 1 : 0;
+
+	const stars = Array.from({ length: fullStars }, (_, index) => (
+    <Icon key={index} name="star" size={14} color="#FFD700" />
+  )).concat(
+    halfStar === 1 ? <Icon key="half-star" name="star-half" size={14} color="#FFD700" /> : null
+  );
+
+	const itemPressed = () => {
+		props.itemPressHandle(props.item)
+	}
+
+  return (
+    <Pressable style={styles.container} onPress={itemPressed}>
+			
+			<Image 
+				source={{ uri: props.item.imageUri }} 
+				style={styles.image}
+			/>
+			
+			<View style={styles.content}>
+				<Text style={styles.text}>{props.item.name}</Text>
+			</View>
+			<View style={styles.starsContainer}>{stars}</View>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+	container: {
+		position: 'relative',
+		alignItems: 'flex-start',
+		justifyContent: 'flex-start',
+		borderWidth: 1.5,
+		borderColor: '#d2b48c',
+		borderRadius: 5,
+		marginHorizontal: 5,
+		overflow: 'hidden',
+		width: 145,
+	},
+	content: {
+		flexDirection: 'row',
+		alignItems: 'center',
+	},
+	text:{
+		padding: 7,
+		fontSize: 16,
+	},
+	starsContainer: {
+    position: 'absolute', 
+    flexDirection: 'row',
+    alignItems: 'center',
+    top: 7, 
+    right: 10, 
+  },
+	image: {
+    width: '100%',
+    height: 210,
+  },
+})
+export default TopTrailsItem;
