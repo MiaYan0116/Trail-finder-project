71fdc6dd844a274ad3c3d6d2cc6ee964d1283b21
mia <yanjiaxin96@gmail.com>
2023-12-13 21:15:00 -0800

    uncomment external api calling functions

--- client/components/StartScreen.js
+++ client/components/StartScreen.js
@@ -8,40 +8,45 @@ import { imageAPIKey, mapAPIKey } from '@env';
 import { Animated } from 'react-native';
 
   
-  // async function fetchTrailGeo(name){
-  //   const apiKey = mapAPIKey;
-  //   const trailName = name;
-  //   try {
-  //     const res = await fetch(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${encodeURIComponent(trailName)}&inputtype=textquery&key=${apiKey}&fields=name,geometry`)
-  //     const data = await res.json();
-  //     const geo = data.candidates[0].geometry.location;
-  //     return geo;
-  //   } catch (error) {
-  //     console.error('Error fetching Place Search API:', error);
-  //   }
-  // }
+async function fetchTrailGeo(name) {
+  const apiKey = mapAPIKey;
+  const trailName = name;
+  try {
+    const res = await fetch(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${encodeURIComponent(trailName)}&inputtype=textquery&key=${apiKey}&fields=name,geometry`);
+    const data = await res.json();
+    if (data.candidates && data.candidates.length > 0 && data.candidates[0].geometry) {
+      const geo = data.candidates[0].geometry.location;
+      return geo;
+    } else {
+      return null; 
+    }
+  } catch (error) {
+    console.error('Error fetching Place Search API:', error);
+  }
+}
 
-  // // We are not using External API at this moment
-  // async function fetchImageUri(trailTitle) {
-  //   try{
-  //     const response = await fetch(`https://api.bing.microsoft.com/v7.0/images/search?q=${trailTitle}`, {
-  //       method: 'GET',
-  //       headers: {
-  //         'Ocp-Apim-Subscription-Key': '8f17ea6d1c9a427aa26d4ff9cbe799b6',
-  //       },
-  //     });
-  //     console.log(response.status);
-  //     const data = await response.json();
-  //     const uri = data.value[0].contentUrl;
-  //     console.log(uri);
-  //     return uri;
-  //   }catch(err){
-  //     console.log(err);
-  //   }
+
+  // We are not using External API at this moment
+  async function fetchImageUri(trailTitle) {
+    try{
+      const response = await fetch(`https://api.bing.microsoft.com/v7.0/images/search?q=${trailTitle}`, {
+        method: 'GET',
+        headers: {
+          'Ocp-Apim-Subscription-Key': '8f17ea6d1c9a427aa26d4ff9cbe799b6',
+        },
+      });
+      // console.log(response.status);
+      const data = await response.json();
+      const uri = data.value[0].contentUrl;
+      // console.log(uri);
+      return uri;
+    }catch(err){
+      console.log(err);
+    }
     
-  // }; 
+  }; 
 
-  // const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+  const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
   const StartScreen = ({ navigation }) => {
     
@@ -63,57 +68,56 @@ import { Animated } from 'react-native';
       }).start();
     }, [slideAnim]);
     
-    // We are not using External API at this moment
-    // useEffect(() => {
-    //   const addInitialData = async (batchSize) => {
-    //     try {
+    useEffect(() => {
+      const addInitialData = async (batchSize) => {
+        try {
 
-    //       const totalBatches = Math.ceil(data.length / batchSize);
-    //       const updatedData = [];
+          const totalBatches = Math.ceil(data.length / batchSize);
+          const updatedData = [];
 
-    //       for (let batchIndex = 0; batchIndex < totalBatches; batchIndex++) {
-    //         const startIndex = batchIndex * batchSize;
-    //         const endIndex = Math.min((batchIndex + 1) * batchSize, data.length);
-    //         const batchItems = data.slice(startIndex, endIndex);
+          for (let batchIndex = 0; batchIndex < totalBatches; batchIndex++) {
+            const startIndex = batchIndex * batchSize;
+            const endIndex = Math.min((batchIndex + 1) * batchSize, data.length);
+            const batchItems = data.slice(startIndex, endIndex);
 
-    //         const batchResults = await Promise.all(batchItems.map(async (item, index) => {
-    //           const retries = 10;
-    //           for (let attempt = 1; attempt <= retries; attempt++) {
-    //             try {
-    //               // if (item.imageUri) {
-    //                 item.imageUri = await fetchImageUri(item.trailTitle);
-    //               // }
-    //               // Call fetchTrailGeo and add the geo property to the item
-    //               const geoData = await fetchTrailGeo(item.trailTitle);
-    //               item.geo = geoData; 
-    //               break;
-    //             } catch (err) {
-    //               console.error(`Error fetching image URI for item ${index}, attempt ${attempt}:`, err);
-    //               if (attempt < retries) {
-    //                 // Retry with a delay (exponential backoff)
-    //                 await delay(2 ** attempt * 1000);
-    //               } else {
-    //                 throw err; // Throw the error if retries are exhausted
-    //               }
-    //             }
-    //           }
-    //           return item;
-    //         }));
+            const batchResults = await Promise.all(batchItems.map(async (item, index) => {
+              const retries = 10;
+              for (let attempt = 1; attempt <= retries; attempt++) {
+                try {
+                  // if (item.imageUri) {
+                    item.imageUri = await fetchImageUri(item.trailTitle);
+                  // }
+                  // Call fetchTrailGeo and add the geo property to the item
+                  const geoData = await fetchTrailGeo(item.trailTitle);
+                  item.geo = geoData; 
+                  break;
+                } catch (err) {
+                  console.error(`Error fetching image URI for item ${index}, attempt ${attempt}:`, err);
+                  if (attempt < retries) {
+                    // Retry with a delay (exponential backoff)
+                    await delay(2 ** attempt * 1000);
+                  } else {
+                    throw err; // Throw the error if retries are exhausted
+                  }
+                }
+              }
+              return item;
+            }));
         
-    //         updatedData.push(...batchResults);
-    //       }
+            updatedData.push(...batchResults);
+          }
 
-    //         // console.log(updatedData);
-    //         for(let i = 0; i < updatedData.length; i++) {
-    //           // console.log(data[i]);
-    //           addInitialDataToFirestore(updatedData[i]);
-    //         }
-    //     } catch (error) {
-    //       console.error("Error updating image URIs:", error);
-    //     }
-    //   }
-    //   addInitialData(14);
-    // }, []);
+            // console.log(updatedData);
+            for(let i = 0; i < updatedData.length; i++) {
+              // console.log(data[i]);
+              addInitialDataToFirestore(updatedData[i]);
+            }
+        } catch (error) {
+          console.error("Error updating image URIs:", error);
+        }
+      }
+      addInitialData(14);
+    }, []);
     
 
     const startHandler = async() => {
